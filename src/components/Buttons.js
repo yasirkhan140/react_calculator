71442bc7d19e18ffdd782cac35ef896487d6f4ee
Yasir khan <129736370+yasirkhan140@users.noreply.github.com>
2023-05-03 22:29:33 +0530

    Do commenting

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,67 +1,86 @@
-import { useState } from "react";
 import React from "react";
 
 function Buttons (props){
-    
-        const {result,onClickBtn,onClickC,onClickEqual,onClickR,onClickPer}= props;
+        // come from props.
+        const {onClickBtn,onClickC,onClickEqual,onClickR,onClickPer}= props;
         return(
+            // main conatiner of btn 
             <div className="main-container">
-                
+                {/* btn div all btn inside this div */}
                 <div className="buttons-div">
+                    {/* btn of clear */}
                     < button className="btn-C" onClick={onClickC}>
                         C
                     </button>
+                    {/* reverse btn and add onClick */}
                     < button className="btn-reverse" onClick ={onClickR}>
                         +/-
                     </button>
+                    {/* percentage btn and add onCLick */}
                     < button className="btn-per" name ="%" onClick={onClickPer} >
                         %
                     </button>
+                    {/* divide btn and add onCLick listener*/}
                     < button className="btn-divide orange" name='/' onClick={onClickBtn}>
                         /
                     </button>
+                    {/* 7 btn and add onCLick */}
                     < button className="btn-7" name ="7" onClick={onClickBtn}>
                         7
                     </button>
+                    {/* 8 btn and add onCLick */}
                     < button className="btn-8" name ="8"onClick={onClickBtn}>
                         8
                     </button>
+                    {/* 9 btn and add onCLick */}
                     < button className="btn-9" name ="9"onClick={onClickBtn}>
                         9
                     </button>
+                    {/* Multiply btn and add onCLick */}
                     < button className="btn-mul orange" name ="*" onClick={onClickBtn}>
                         *
                     </button>
+                    {/* 4 btn and add onCLick */}
                     < button className="btn-4" value ="4"onClick={onClickBtn}>
                         4
                     </button>
+                    {/* 5 btn and add onCLick */}
                     < button className="btn-5" name ="5"onClick={onClickBtn}>
                         5
                     </button>
+                    {/* 6 btn and add onCLick */}
                     < button className="btn-6" name ="6"onClick={onClickBtn}>
                         6
                     </button>
+                    {/* minus btn and add onCLick */}
                     < button className="btn-min orange" name ="-"onClick={onClickBtn} >
                         -
                     </button>
+                    {/* 1 btn and add onCLick */}
                     < button className="btn-1" value ="1" name = "1"onClick={onClickBtn}>
                         1
                     </button>
+                    {/* 2 btn and add onCLick */}
                     < button className="btn-2" value ="2" name = "2"onClick={onClickBtn}>
                         2
                     </button>
+                    {/* 3 btn and add onCLick */}
                     < button className="btn-3" value ="3" name = "3"onClick={onClickBtn}>
                         3
                     </button>
+                    {/* plus btn and add onCLick */}
                     < button className="btn-plus orange" name ="+" onClick={onClickBtn}>
                         +
                     </button>
+                    {/* 0 btn and add onCLick */}
                     < button className="btn-0" value ="0" name = "0" onClick={onClickBtn}>
                         0
                     </button>
+                    {/* point btn and add onCLick */}
                     < button className="btn-deci" name ="." onClick={onClickBtn}>
                         .
                     </button>
+                    {/* equal btn and add onCLick */}
                     < button className="btn-equal orange" name ="=" onClick={onClickEqual}>
                         =
                     </button>
