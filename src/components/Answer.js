adedd06c2013880ac515a2577b553c60af0f73ed
Yasir khan <129736370+yasirkhan140@users.noreply.github.com>
2023-05-03 22:28:47 +0530

    do comminting

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,7 +1,11 @@
+// this is for only show a result.
 function Answer(props){
+    // take result from props.
     const {result} = props
+
     return(
         <div className="answer-div">
+            {/* input set value result */}
             <input value ={result} className="result-show"/>
         </div>
 
